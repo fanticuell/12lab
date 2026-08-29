1e761c257148f0602d7270b9411a2d27b37dbbf2
24030 <24030@WS-2902>
2026-08-29 08:43:30 +0500

    Готовое задание 1

---
+++ PlacesLibrary/MakeList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlacesLibrary
+{
+    public class MakeList
+    {
+        public Node Head { get; private set; }
+        public Node Tail { get; private set; }
+        public int Count { get; private set; }
+
+        public void AddLast(Place data)
+        {
+            Node newNode = new Node(data);
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                Tail.Next = newNode;
+                newNode.Prev = Tail;
+                Tail = newNode;
+            }
+            Count++;
+        }
+
+        public void PrintList(string message)
+        {
+            Console.WriteLine($"\n--- {message} ---");
+            if (Head == null)
+            {
+                Console.WriteLine("Список пуст.");
+                return;
+            }
+
+            Node current = Head;
+            int index = 1;
+            while (current != null)
+            {
+                Console.Write($"[{index}] ");
+                current.Data.Show();
+                current = current.Next;
+                index++;
+            }
+        }
+
+        public void RemoveEvenPositions()
+        {
+            if (Head == null) return;
+
+            Node current = Head;
+            int currentPosition = 1;
+
+            while (current != null)
+            {
+                Node nextNode = current.Next;
+
+                if (currentPosition % 2 == 0)
+                {
+                    if (current == Tail)
+                    {
+                        Tail = current.Prev;
+                    }
+
+                    if (current.Prev != null) current.Prev.Next = current.Next;
+                    if (current.Next != null) current.Next.Prev = current.Prev;
+
+                    current.Next = null;
+                    current.Prev = null;
+
+                    Count--;
+                }
+
+                current = nextNode;
+                currentPosition++;
+            }
+        }
+
+        public void Clear()
+        {
+            Node current = Head;
+            while (current != null)
+            {
+                Node next = current.Next;
+                current.Next = null;
+                current.Prev = null;
+                current.Data = null;
+
+                current = next;
+            }
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,176 +13,43 @@ namespace _10
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-
-            Place[] places = new Place[5];
-
-            for (int i = 0; i < places.Length; i++)
+            Console.WriteLine("Введите номер задания");
+            string zadanie = Console.ReadLine();
+            Console.WriteLine();
+            if (zadanie == "1")
             {
-                int type = rnd.Next(5);
+                MakeList list = new MakeList();
+                list.AddLast(new Place("Центральный Парк"));
+                list.AddLast(new Region("Московская область", "Center"));
+                list.AddLast(new City("Казань", "Center2", 1234321));
+                list.AddLast(new Megapolis("Токио", "Center3", 666666, true));
+                list.AddLast(new Place("Секретное место"));
+                list.AddLast(new City("Пермь", "Center4", 1020400));
 
-                switch (type)
-                {
-                    case 0:
-                        places[i] = new Place();
-                        break;
-                    case 1:
-                        places[i] = new Region();
-                        break;
-                    case 2:
-                        places[i] = new City();
-                        break;
-                    case 3:
-                        places[i] = new Megapolis();
-                        break;
-                    case 4:
-                        places[i] = new Address();
-                        break;
-                }
+                list.PrintList("Исходный список (Пункт 2)");
 
-                places[i].RandomInit();
+                list.RemoveEvenPositions();
+                Console.WriteLine("\n[Система]: Произведено удаление элементов на четных позициях.");
+
+                list.PrintList("Список после удаления четных элементов (Пункт 4)");
+
+                list.Clear();
+
+                list.PrintList("Проверка списка после очистки memory");
             }
-
-            Console.WriteLine("=== Обычный вызов ===");
-            foreach (Place p in places)
+            else if (zadanie == "2")
             {
-                p.Show();
-                Console.WriteLine();
+
             }
-
-            Array.Sort(places);
-
-            Console.WriteLine("\n=== После сортировки по Name ===");
-            foreach (var p in places)
+            else if (zadanie == "3")
             {
-                p.Show();
-                Console.WriteLine();
+
             }
-
-            Array.Sort(places, new PopulationComparer());
-
-            Console.WriteLine("\n=== Сортировка по населению ===");
-            foreach (var p in places)
+            else if (zadanie == "4")
             {
-                p.Show();
-                Console.WriteLine();
-            }
 
-            Place target = new Place("Место1");
-
-            int index = Array.BinarySearch(places, target);
-
-            if (index >= 0)
-            {
-                Console.WriteLine("\nНайден элемент:");
-                places[index].Show();
-            }
-            else
-            {
-                Console.WriteLine("\nЭлемент не найден");
-            }
-
-            IInit[] arr = new IInit[5];
-
-            arr[0] = new Place();
-            arr[1] = new City();
-            arr[2] = new Megapolis();
-            arr[3] = new Address();
-            arr[4] = new RandomClass();
-
-            Console.WriteLine("\n=== RandomInit для IInit ===");
-
-            foreach (var obj in arr)
-            {
-                obj.RandomInit();
-
-                if (obj is Place p)
-                    p.Show();
-                else if (obj is RandomClass rn)
-                    rn.Show();
-            }
-
-            Place original = new Place();
-            original.RandomInit();
-            original.Addr = new Address();
-            original.Addr.RandomInit();
-
-            Place clone = (Place)original.Clone();
-            Place shallow = original.ShallowCopy();
-
-            Console.WriteLine("\n=== Клонирование ===");
-
-            original.Addr.Street = "Ленина2";
-
-            Console.WriteLine("\nПосле изменения оригинала:");
-
-            original.Addr.Show();
-            shallow.Addr.Show();
-            clone.Addr.Show();
-
-            //Console.Write("\nВведите центр области: ");
-            //string center = Console.ReadLine();
-
-            //Console.Write("Введите минимальное население: ");
-            //int minPop = int.Parse(Console.ReadLine());
-
-            //Console.Write("Введите улицу: ");
-            //string street = Console.ReadLine();
-
-            //CitiesByRegion(places, center);
-            //MegapolisByPopulation(places, minPop);
-            //CountAddressesByStreet(places, street);
-        }
-
-        static void CitiesByRegion(Place[] places, string regionCenter)
-        {
-            Console.WriteLine($"\nГорода области с центром: {regionCenter}");
-
-            foreach (Place p in places)
-            {
-                if (p is City city)
-                {
-                    if (city.RegionCenter == regionCenter)
-                    {
-                        Console.WriteLine(city.Name);
-                    }
-                }
             }
         }
 
-        static void MegapolisByPopulation(Place[] places, int minPopulation)
-        {
-            Console.WriteLine($"\nМегаполисы с населением больше {minPopulation}:");
-
-            foreach (Place p in places)
-            {
-                if (p is Megapolis mega)
-                {
-                    if (mega.Population > minPopulation)
-                    {
-                        mega.Show();
-                        Console.WriteLine();
-                    }
-                }
-            }
-        }
-
-        static void CountAddressesByStreet(Place[] places, string street)
-        {
-            int count = 0;
-
-            foreach (Place p in places)
-            {
-                if (p is Address addr)
-                {
-                    if (addr.Street == street)
-                    {
-                        count++;
-                    }
-                }
-            }
-
-            Console.WriteLine($"\nКоличество адресов на улице {street}: {count}");
-        }
     }
 }
